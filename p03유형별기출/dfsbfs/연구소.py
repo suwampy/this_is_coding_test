b46f5ab9ee9657873c2d834fa22a7e377576aa5f
suwampy <tndhks123@nate.com>
2022-09-05 00:40:24 +0900

    dfs 연구소

---
+++ p03유형별기출/dfsbfs/연구소.py
@@ -0,0 +1,67 @@
+'''
+https://www.acmicpc.net/problem/14502
+가능한 모든 경우의 수를 계산하되, 안전 영역을 계산할 때 DFS나 BFS를 적절히 이용해야 함
+DFS 혹은 BFS를 사용하여 완전 탐색을 수행해야 함...
+'''
+n, m = map(int, input().split()) # 세로 n 가로 m
+data = [] # 초기 맵 리스트
+temp= [[0] * m for _ in range[n]] # 벽을 설치한 뒤의 맵 리스트
+
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+# 4가지 이동 방향에 대한 리스트
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+result = 0
+
+# 깊이 우선 탐색[DFS)를 이용해 각 바이러스가 사방으로 퍼지도록 하기
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
+        if temp[nx][ny] == 0:
+            # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
+            temp[nx][ny] = 2
+            virus(nx, ny)
+
+# 현재 맵에서 안전 영역의 크기 계산하는 메서드
+def get_source():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+# 깊이 우선 탐색(DFS)을 이용해 울타리를 설치하면서, 매번 안전 영역의 크기 계산
+def dfs(count):
+    global result
+    # 울타리가 3개 설치된 경우
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+        # 각 바이러스의 위치에서 전파 진행
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+        #안전 영역의 최댓값 계산
+        result = max(result, get_source())
+        return
+    # 빈 공간에 울타리 설치
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+
+dfs(0)
+print(result)
+
